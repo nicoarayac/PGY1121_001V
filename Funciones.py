50d9e6c190099caf767c64184c28de0618899ea6
Nicolas Aaraya <nicola.arayac@duocuc.cl>
2023-05-27 00:57:47 +0000

    Fuiones Clkases 3.3

---
+++ Funciones.py
@@ -0,0 +1,16 @@
+def saludar ():
+    print("Hola Gottmik")
+
+def sumar_1():
+    num1 = 2
+    num2 = 3
+    return (num1 + num2)
+
+def sumar(a,b):
+    suma = a + b
+    return  suma
+print(f"la suma de dos numeros es: {suma}")
+
+  
+
+
